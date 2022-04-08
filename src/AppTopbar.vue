453441c2fcaf3443c5a45e31c842d384396b6072
zxl <1402050236@qq.com>
2022-04-08 13:53:14 +0800

    分析AppTopbar的代码

--- src/AppTopbar.vue
+++ src/AppTopbar.vue
@@ -1,58 +1,67 @@
 <template>
-	<div class="layout-topbar">
-		<router-link to="/" class="layout-topbar-logo">
-			<img alt="Logo" :src="topbarImage()" />
-			<span>SAKAI</span>
-		</router-link>
-		<button class="p-link layout-menu-button layout-topbar-button" @click="onMenuToggle">
-			<i class="pi pi-bars"></i>
-		</button>
+  <div class="layout-topbar">
 
-		<button class="p-link layout-topbar-menu-button layout-topbar-button"
-			v-styleclass="{ selector: '@next', enterClass: 'hidden', enterActiveClass: 'scalein', 
+    <!-- 顶部的logo信息 -->
+    <router-link to="/" class="layout-topbar-logo">
+      <img alt="Logo" :src="topbarImage()"/>
+      <span>物资</span>
+    </router-link>
+
+    <!-- 顶部切换页面大小的按钮 -->
+    <button class="p-link layout-menu-button layout-topbar-button" @click="onMenuToggle">
+      <i class="pi pi-bars"></i>
+    </button>
+
+    <!-- 这是什么按钮？ -->
+    <button class="p-link layout-topbar-menu-button layout-topbar-button"
+            v-styleclass="{ selector: '@next', enterClass: 'hidden', enterActiveClass: 'scalein',
 			leaveToClass: 'hidden', leaveActiveClass: 'fadeout', hideOnOutsideClick: true}">
-			<i class="pi pi-ellipsis-v"></i>
-		</button>
-		<ul class="layout-topbar-menu hidden lg:flex origin-top">
-			<li>
-				<button class="p-link layout-topbar-button">
-					<i class="pi pi-calendar"></i>
-					<span>Events</span>
-				</button>
-			</li>
-			<li>
-				<button class="p-link layout-topbar-button">
-					<i class="pi pi-cog"></i>
-					<span>Settings</span>
-				</button>
-			</li>
-			<li>
-				<button class="p-link layout-topbar-button">
-					<i class="pi pi-user"></i>
-					<span>Profile</span>
-				</button>
-			</li>
-		</ul>
-	</div>
+      <i class="pi pi-ellipsis-v"></i>
+    </button>
+
+    <!-- 顶部右侧的三个按钮 span中的描述也没有在页面上显示啊？-->
+    <ul class="layout-topbar-menu hidden lg:flex origin-top">
+      <li>
+        <button class="p-link layout-topbar-button">
+          <i class="pi pi-calendar"></i>
+          <span>日历</span>
+        </button>
+      </li>
+      <li>
+        <button class="p-link layout-topbar-button">
+          <i class="pi pi-cog"></i>
+          <span>设置中心</span>
+        </button>
+      </li>
+      <li>
+        <button class="p-link layout-topbar-button">
+          <i class="pi pi-user"></i>
+          <span>用户中心</span>
+        </button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    methods: {
-        onMenuToggle(event) {
-            this.$emit('menu-toggle', event);
-        },
-		onTopbarMenuToggle(event) {
-            this.$emit('topbar-menu-toggle', event);
-        },
-		topbarImage() {
-			return this.$appState.darkTheme ? 'images/logo-white.svg' : 'images/logo-dark.svg';
-		}
+  name:'AppTopbar',
+  //方法加个备注？
+  methods: {
+    onMenuToggle(event) {
+      this.$emit('menu-toggle', event);
     },
-	computed: {
-		darkTheme() {
-			return this.$appState.darkTheme;
-		}
-	}
+    onTopbarMenuToggle(event) {
+      this.$emit('topbar-menu-toggle', event);
+    },
+    topbarImage() {
+      return this.$appState.darkTheme ? 'images/logo-white.svg' : 'images/logo-dark.svg';
+    }
+  },
+  computed: {
+    darkTheme() {
+      return this.$appState.darkTheme;
+    }
+  }
 }
 </script>
