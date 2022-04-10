8ad369b64f4bad68bb5feec0b8bd9a923bd1df20
Lxd-ncwu <1750402901@qq.com>
2022-04-10 16:55:25 +0800

    采购具体模块以及需要实现的细节

--- src/AppTopbar.vue
+++ src/AppTopbar.vue
@@ -27,6 +27,7 @@
     </button>
 
     <!-- 顶部右侧的三个按钮 span中的描述也没有在页面上显示啊？-->
+    <!-- 在右上角的头像那里 点击出现登出和修改密码弹框 -->
     <ul class="layout-topbar-menu hidden lg:flex origin-top">
       <li>
         <button class="p-link layout-topbar-button">
